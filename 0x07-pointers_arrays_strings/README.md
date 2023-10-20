98daabd17a8de1127a7ed128e2b276069f21312e
meenahkenz <itzamina65@gmail.com>
2023-10-20 23:10:13 +0100

    #include main.h

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+i
+0. memset
+
+Write a function that fills memory with a constant byte.
+
+1. memcpy
+
+Write a function that copies memory area.
+
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unisted.h>
+
+/**
+ 
+  * _putchar - writes the character c to stdout
+  * @c : The character to print
+  *
+  * Return: On sucess 1.
+  * On error, -1 is returned, and error is set appropriately.
+  */
+int _putchar(char c)
+{        
+	return(write(1, &c, i));
+}#include "main.h"
+#include <unisted.h>
+
+/**
+ 
+  * _putchar - writes the character c to stdout
+  * @c : The character to print
+  *
+  * Return: On sucess 1.
+  * On error, -1 is returned, and error is set appropriately.
+  */
+int _putchar(char c)
+{        
+	return(write(1, &c, i));
+}
