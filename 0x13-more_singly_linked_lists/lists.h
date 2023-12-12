c72a91ed9783deff69eb3f7191ef926481f08397
meenahkenz <itzamina65@gmail.com>
2023-12-12 03:49:54 +0100

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,6 +21,7 @@ struct listint_s *next;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+size_t free_listint_safe(listint_t **h);
 size_t print_listint_safe(const listint_t *head);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
