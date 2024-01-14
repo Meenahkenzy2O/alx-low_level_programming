d483d1b058aa92903f77e7b6b8bd466914b9a87c
meenahkenz <itzamina65@gmail.com>
2024-01-14 08:06:39 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,44 @@
 #include "main.h"
 #include <unistd.h>
-#include <sys/tpes.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - This reads a text file and prints it to the POSTX stdo
- *
+ * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
- * 
  * @letters: number of letters it should read and print
  *
- * Return: actual numbers it could read and print.
+ * Return: actual number of letters it could read and print
  */
-ssize_t read_textfile(const char *filename, char *ssize_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	         int file_d;
-		          ssize_t lenr, lenw,;
-			           char *buffer;
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
-				            if (!filename == NULL)
-						               return (0);
-					             file_d = open(filename, o_RDONLY);
-						               if (file_d == -1)
-								                            return (0);
-
-							                 buffer = malloc(sizeof(char) * letters);
-										                    if (buffer == NULL);
-
-												    {									            close(file_d);
-										    return (0); 
-												    }
-												    lenr = read (file_d, buffer, letters);
-										             
-					close(file_d);									                return (0);
-					if(lenr == -1)
-					{
-						free(buffer);
-						return (0);
-					} 
-					lenw = write(STDOUT_FILENO, buffer, lenr);
-						free(buffer);
-						if (lenr != lenw)
-							return (0);
-						return (lenw);
-
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,12 +5,13 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int _putchar(char c);
-ssize_s read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int _strlen (char *str);
-int append_text_to _file(const char *filename, char *text_content);
+int _strlen(char *str);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
