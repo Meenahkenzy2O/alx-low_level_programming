9787cc64ae0997fc4821d1c7a4ad6ecf511fb1ac
meenahkenz <itzamina65@gmail.com>
2024-01-14 08:43:39 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,38 @@
 #include "main.h"
 
 /**
- * create_file - prog creates a file
+ * create_file - creates a file
  * @filename: filename.
- * @text_content : content writed in the file.
+ * @text_content: content writed in the file.
  *
- * Return: 1 if it sucess. -1 if it fails.
+ * Return: 1 if it success. -1 if it fails.
  */
-int creat_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	 int file_d;
-	 int nletters;
-	 int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-	 if (!filename)
-	   return (-1);
-	 file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	  if (file_d == -1)
-		     return (-1);
+	if (!filename)
+		return (-1);
 
-	  if (!text_content)
-		   text_content = "";
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	   for (nletters = 0; text_content[nletters]; nletters++);
-	   
-	   rwr = write(file_d, text_content, nletters);
-	   
-	   if (rwr == -1)
-		   return (-1);
-	   close(file_d);
+	if (fd == -1)
+		return (-1);
 
-	   return (1);
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -1,22 +1,22 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(int ac, char **av)
 {
-	    int res;
+	int res;
 
-	        if (ac != 3)
-			    {
-				            dprintf(2, "Usage: %s filename text\n", av[0]);
-					            exit(1);
-						        }
-		    res = create_file(av[1], av[2]);
-		        printf("-> %i)\n", res);
-			    return (0);
+	if (ac != 3)
+	{
+	dprintf(2, "Usage: %s filename text\n", av[0]);
+	exit(1);
+	}
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
